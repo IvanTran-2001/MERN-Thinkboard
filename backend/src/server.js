49c18de896bb01f1f6fa184188873089570d4f78
mysto <mystoganx2001@gmail.com>
2026-02-05 10:07:04 +1100

    Adding Util for Date Conversion. Created Note sections

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,19 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 9999;
 
+app.use(cors({
+  origin: "http://localhost:5173", // Allow requests from this origin
+
+}));
 app.use(express.json()); // TThis middleware to parse JSON bodies
-
 app.use(rateLimiter);
-
 // app.use((req,res,next) => {
 //   console.log(`Req Method is ${req.method} & Req URL is ${req.path}`);
 //   next();
