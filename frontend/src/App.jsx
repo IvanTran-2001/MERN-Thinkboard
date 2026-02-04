4de7f2ea84f5204c44642312a95ca354dc8b7901
mysto <mystoganx2001@gmail.com>
2026-02-04 19:45:45 +1100

    Adding tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Routes } from 'react-router'
+import { Routes, Route } from 'react-router'
 import HomePage from './pages/HomePage.jsx'
 import CreatePage from './pages/CreatePage'
 import NoteDetailPage from './pages/NoteDetailPage.jsx'
+import toast from 'react-hot-toast'
 
 const App = () => {
   return (
     <div>
+      <button onClick={() => toast.success('Button clicked!')}>Click me</button>
       <Routes>
         <Route path = "/" element={<HomePage />} />
         <Route path = "/create" element={<CreatePage />} />
