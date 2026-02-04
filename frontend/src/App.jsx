63e7b72b44ff9f41d962a3af973b09af48d014a7
mysto <mystoganx2001@gmail.com>
2026-02-04 20:22:36 +1100

    Changing background color to lemon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
 import { Routes, Route } from 'react-router'
 import HomePage from './pages/HomePage.jsx'
 import CreatePage from './pages/CreatePage'
 import NoteDetailPage from './pages/NoteDetailPage.jsx'
-import toast from 'react-hot-toast'
+// import toast from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div>
-      <button onClick={() => toast.success('Button clicked!')}>Click me</button>
+    <div data-theme="lemonade">
+      <button className="btn btn-outline">Click Me</button>
       <Routes>
         <Route path = "/" element={<HomePage />} />
         <Route path = "/create" element={<CreatePage />} />
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,14 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  }, 
-  plugins: [],
+  plugins: [daisyui],
+  
+  daisyui: {
+    themes: ["lemonade", "cupcake", "synthwave"],
+  },
 }
