7273828bbf6e7064a6c007be76a23f4f6f58f1c9
mysto <mystoganx2001@gmail.com>
2026-02-04 17:52:46 +1100

    Fixing rate limiter and putting ConnectDB in correct order of server.js

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -1,13 +1,13 @@
-import {Ratelimiter} from '@upstash/ratelimit'
-import {Redis} from '@upstash/redis'
+import { Ratelimit } from '@upstash/ratelimit'
+import { Redis } from '@upstash/redis'
 
 import dotenv from "dotenv";
 dotenv.config();
 
 // Create a new ratelimiter, that allows 10 requests per 20 seconds 
-const ratelimit = new Ratelimiter({
+const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(10, '20 s'),
+  limiter: Ratelimit.slidingWindow(100, '60 s'),
 })
 
 export default ratelimit;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,16 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
 app.use(express.json()); // TThis middleware to parse JSON bodies
+
+app.use(rateLimiter);
 
 // app.use((req,res,next) => {
 //   console.log(`Req Method is ${req.method} & Req URL is ${req.path}`);
@@ -19,6 +20,10 @@ app.use(express.json()); // TThis middleware to parse JSON bodies
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server started on PORT: ${PORT}`)
-});   
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started on PORT: ${PORT}`)
+  });  
+});
+
+ 
