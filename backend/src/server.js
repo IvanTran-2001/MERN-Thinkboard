281c6d6c53f8aa489f8c8fae334a1adc06847ffc
mysto <mystoganx2001@gmail.com>
2026-02-05 15:47:36 +1100

    Prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,24 +4,37 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 9999;
+const __dirname = path.resolve();
 
-app.use(cors({
-  origin: "http://localhost:5173", // Allow requests from this origin
-
-}));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({
+    origin: "http://localhost:5173", // Allow requests from this origin
+  }));
+}
 app.use(express.json()); // TThis middleware to parse JSON bodies
 app.use(rateLimiter);
+
 // app.use((req,res,next) => {
 //   console.log(`Req Method is ${req.method} & Req URL is ${req.path}`);
 //   next();
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+// In production, the frontend will be served from the same origin as the backend.
+const BASE_URL = import.meta.env.MODE === "production" ? "http://localhost:5001" : "/api";
 const api = axios.create({
-  baseURL: "http://localhost:5001/api"
+  baseURL: BASE_URL,
 });
 
 export default api;
