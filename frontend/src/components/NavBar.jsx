2417be1e2e097c9ea6a5e5d374d81f43be106d78
mysto <mystoganx2001@gmail.com>
2026-02-07 09:00:01 +1100

    Adding a profile dropbar includes: Profile, settings and logout

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,15 @@
+import React from "react";
 import { Link } from "react-router";
 import { PlusIcon } from "lucide-react";
 
 const NavBar = () => {
+
+  const [userName, _] = React.useState("User"); // Placeholder for user name
+  
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.href = "/"; // Redirect to login page after logout
+  }
   return (
     <header className="bg-base-300 border-b border-base-content/10">
       <div className="mx-auto max-w-6xl p-4">
@@ -14,6 +22,16 @@ const NavBar = () => {
               <PlusIcon className="size-5" />
               <span>New Note</span>
             </Link>
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost">
+                {userName} {/* or user icon */}
+              </label>
+              <ul className="dropdown-content menu p-2 shadow bg-base-100 rounded-box">
+                <li><a>Profile</a></li>
+                <li><a>Settings</a></li>
+                <li><button onClick={handleLogout}>Logout</button></li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
