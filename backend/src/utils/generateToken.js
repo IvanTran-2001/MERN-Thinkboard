1ca495f636ceb6910c5efb9370b52b9d5953167d
mysto <mystoganx2001@gmail.com>
2026-02-06 14:18:17 +1100

    Tokens last 7 days

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -2,6 +2,6 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (userId) => {
   return jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
+    expiresIn: "7d",
   });
 };
